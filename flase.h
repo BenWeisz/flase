638774aea14071c460a0003c70c211fac6e06a90
BenWeisz <bencew99@gmail.com>
2024-07-27 00:28:59 -0700

    Add option to override printf

--- flase.h
+++ flase.h
@@ -1,7 +1,10 @@
 #ifndef FLASE_H
 #define FLASE_H
 
+#ifndef FLASE_PRINT
 #include <stdio.h>
+#define FLASE_PRINT printf
+#endif
 
 #define FLASE_DEFINE_TEST( name ) \
 	unsigned int flase_test_an_##name = 0; \
@@ -10,16 +13,16 @@
 #define FLASE_RUN_TEST( name ) \
 	({ unsigned int flase_test_pn_##name = 0; \
 		do { \
-			printf( "FLASE Run Test: " #name "\n" ); \
+			FLASE_PRINT( "FLASE Run Test: " #name "\n" ); \
 			flase_test_##name( &flase_test_an_##name, &flase_test_pn_##name ); \
-			printf( "FLASE Report: " #name " | "); \
+			FLASE_PRINT( "FLASE Report: " #name " | "); \
 			if ( flase_test_pn_##name == flase_test_an_##name ) \
 			{ \
-				printf( "\e[1;32mPASS\e[0m [\e[1;32m%d/%d\e[0m]\n", flase_test_pn_##name, flase_test_an_##name ); \
+				FLASE_PRINT( "\e[1;32mPASS\e[0m [\e[1;32m%d/%d\e[0m]\n", flase_test_pn_##name, flase_test_an_##name ); \
 			} \
 			else \
 			{ \
-				printf( "\e[1;31mFAIL\e[0m [\e[1;31m%d/%d\e[0m]\n", flase_test_pn_##name, flase_test_an_##name ); \
+				FLASE_PRINT( "\e[1;31mFAIL\e[0m [\e[1;31m%d/%d\e[0m]\n", flase_test_pn_##name, flase_test_an_##name ); \
 			} \
 		} while(0); \
 	})
@@ -29,7 +32,7 @@
 		do { \
 			if ( !( cond ) ) \
 			{ \
-				printf( "\t\e[1;31mFailed:\e[0m %s, %s:%d\n", (msg), __FILE__, __LINE__ ); \
+				FLASE_PRINT( "\t\e[1;31mFailed:\e[0m %s, %s:%d\n", (msg), __FILE__, __LINE__ ); \
 			} \
 			else \
 			{ \
